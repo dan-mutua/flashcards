7ee5b6ac8ed8582fb3529de61b0a9ff0e1fa8c0e
dan-mutua <daniel.mutua@student.moringaschool.com>
2021-10-28 23:34:02 +0300

    user registration

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.generics import GenericAPIView
 from users.serializers import RegisterSerializer
+from rest_framework import response,status
 
 # Create your views here.
 class RegisterAPIView(GenericAPIView):
@@ -8,3 +9,9 @@ class RegisterAPIView(GenericAPIView):
 
   def post(self, request):
     serializers=self.serializer_class(data=request.data)
+
+    if serializers.is_valid():
+      serializers.save()
+      return response.Response(serializers.data,status=status.HTTP_201_CREATED)
+
+    return response.Response(serializers.errors,status=status.HTTP_400_BAD_REQUEST)  
